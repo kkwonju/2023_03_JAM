a17b26f4dbd7197b74f34b8220381a566d064822
kkwonju <kkwo4140@gmail.com>
2023-03-17 16:48:03 +0900

    JAM 시작

---
+++ src/com/koreaIT/example/JAM/Article.java
@@ -0,0 +1,13 @@
+package com.koreaIT.example.JAM;
+
+public class Article extends Object {
+	int id;
+	String title;
+	String body;
+
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+}
