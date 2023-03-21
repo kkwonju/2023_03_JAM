312955c475ee9baf2c84496fe421f6ff5965bd7d
kkwonju <kkwo4140@gmail.com>
2023-03-21 13:38:01 +0900

    게시물 작성 시 DB에 INSERT

--- src/com/koreaIT/example/JAM/Main.java
+++ src/com/koreaIT/example/JAM/Main.java
@@ -1,5 +1,9 @@
 package com.koreaIT.example.JAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -27,6 +31,7 @@ public class Main {
 			}
 
 			if (command.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
 				int id = lastArticleId + 1;
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
@@ -36,7 +41,52 @@ public class Main {
 				articles.add(new Article(id, title, body));
 				System.out.println(id + "번 글이 생성되었습니다");
 				lastArticleId++;
+				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 
+					conn = DriverManager.getConnection(url, "root", ""); // root : mysql 기본 계정 , "" : 비밀번호
+					System.out.println("연결 성공!");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW(),";  // " SET , 문자열 간 공백, 붙으면 하나로 인식
+					sql += "updateDate = NOW(),";
+					sql += "title = '" + title + "',";
+					sql += "`body` = '" + body + "'";
+
+					System.out.println(sql);
+					
+					pstmt = conn.prepareStatement(sql);
+					
+					int affectedRow= pstmt.executeUpdate();
+
+					System.out.println("affectedRow : " + affectedRow);
+				
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 			} else if (command.equals("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시글이 존재하지 않습니다");
